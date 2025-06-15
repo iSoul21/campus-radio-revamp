926c7272bbcc33afc9568538f71bd0953b4b66d1
Prince Lucban <116994228+PrinceJL@users.noreply.github.com>
2025-06-16 07:57:57 +0800

    Docker integration fix, Camera is now working for the broadcaster, need to integrate

--- server/blueprints/frontend.py
+++ server/blueprints/frontend.py
@@ -3,7 +3,7 @@ import os
 
 frontend_bp = Blueprint("frontend", __name__)
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../app/apps"))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../apps"))
 
 @frontend_bp.route("/")
 def index():
